1b4c8d68e92790ffb9f7424de1619407ae76a834
Tim Caswell <tim@creationix.com>
2012-11-22 21:40:38 -0600

    Add unit test for autoheaders middleware

---
+++ ensure.lua
@@ -0,0 +1,69 @@
+local p = require('utils').prettyPrint
+local wrap = require('fiber').new
+local pathKey = {} -- key for paths
+
+local pass = "\27[1;32m◀\27[0mPASS\27[1;32m▶\27[0m\27[0;32m "
+local fail = "\27[1;31m◀\27[0mFAIL\27[1;31m▶\27[0m\27[0;31m "
+local tests
+local index = 1
+
+local function run()
+  local test = tests[index]
+  if not test then
+    print("All tests done")
+    return
+  end
+  index = index + 1
+
+  wrap(test.block, function (err)
+    print(pass .. test.name .. "\27[0m")
+    run()
+  end)(function (err)
+    if err then
+      print(fail .. test.name .. "\27[0m")
+      print(err)
+      run()
+    end
+  end)
+
+end
+
+local function describe(name, block)
+  local isOuter = not tests
+  if isOuter then
+    tests = {}
+  end
+  local parentenv = getfenv(block)
+  local parentPath = parentenv[pathKey]
+  local path = parentPath and (parentPath .. " - " .. name) or name
+  local env = setmetatable({
+    [pathKey] = path,
+    describe = describe,
+    it = function (name, block)
+      table.insert(tests, {name=path .. " - " .. name, block=block})
+    end
+  }, { __index = parentenv })
+  setfenv(block, env)
+  block()
+  if isOuter then
+    run()
+  end
+end
+
+local function same(a, b)
+  if a == b then return true end
+  if not (type(a) == "table" and type(b) == "table") then return false end
+  for k, v in pairs(a) do
+    if not same(b[k], v) then return false end
+  end
+  for k in pairs(b) do
+    if not a[k] then return false end
+  end
+  return true
+end
+
+
+return {
+  describe = describe,
+  same = same
+}
---
+++ tests/test-autoheaders.lua
@@ -0,0 +1,233 @@
+
+package.path = "../?.lua;" .. package.path
+package.cpath = "../?.so;" .. package.cpath
+local autoheaders = require('autoheaders')
+local newStream = require('stream').newStream
+local await = require('fiber').await
+local p = require('utils').prettyPrint
+local nextTick = require('continuable').nextTick
+local flushTickQueue = require('continuable').flushTickQueue
+local describe = require('ensure').describe
+local same = require('ensure').same
+
+describe("autoheaders", function ()
+
+  it("is a function", function (done)
+    assert(type(autoheaders) == "function")
+    done()
+  end)
+
+  describe("string body", function ()
+    local app = function (req, res)
+      res(200, {
+        ["Content-Type"] = "text/plain"
+      }, "Hello World\n")
+    end
+    local request = {
+      method = "GET",
+      url = { path = "/" },
+      headers = {}
+    }
+
+    it("proxies to main app", function (done)
+      autoheaders(app)(request, function (code, headers, body)
+        assert(code == 200)
+        assert(body == "Hello World\n")
+      end)
+      done()
+    end)
+
+    it("adds Server header", function (done)
+      autoheaders(app, {autoServer="BustedServer"})(request, function (code, headers, body)
+        assert(headers["Server"] == "BustedServer")
+      end)
+      done()
+    end)
+
+    it("adds Date header", function (done)
+      autoheaders(app)(request, function (code, headers, body)
+        assert(type(headers["Date"]) == "string")
+      end)
+      done()
+    end)
+
+    it("adds Content-Length header", function (done)
+      autoheaders(app)(request, function (code, headers, body)
+        assert(headers["Content-Length"] == #body)
+      end)
+      done()
+    end)
+
+    it("Adds Connection: close", function (done)
+      autoheaders(app)(request, function (code, headers, body)
+        assert(headers["Connection"]:lower() == "close")
+      end)
+      done()
+    end)
+
+    it("Adds Connection: keep-alive", function (done)
+      local request = {
+        method = "GET",
+        url = { path = "/" },
+        should_keep_alive = true,
+        headers = {
+          Connection = "Keep-Alive"
+        }
+      }
+      autoheaders(app)(request, function (code, headers, body)
+        assert(headers["Connection"]:lower() == "keep-alive")
+      end)
+      done()
+    end)
+
+    it("should do chunked encoding", function (done)
+      autoheaders(app, {autoContentLength=false})(request, function (code, headers, body)
+        assert(headers["Transfer-Encoding"] == "chunked")
+        assert(headers["Content-Length"] == nil)
+        assert(same(body, {
+          "C\r\n",
+          "Hello World\n",
+          "\r\n0\r\n\r\n"
+        }))
+      end)
+      done()
+    end)
+
+  end)
+
+  describe("array body", function ()
+    local app = function (req, res)
+      res(200, {
+        ["Content-Type"] = "text/plain"
+      }, {"Hello ", "World\n"})
+    end
+    local request = {
+      method = "GET",
+      url = { path = "/" },
+      headers = {}
+    }
+
+    it("should add Content-Length header", function (done)
+      autoheaders(app)(request, function (code, headers, body)
+        assert(headers["Content-Length"] == 12)
+      end)
+      done()
+    end)
+
+    it("should do chunked encoding", function (done)
+      autoheaders(app, {autoContentLength=false})(request, function (code, headers, body)
+        assert(headers["Transfer-Encoding"] == "chunked")
+        assert(headers["Content-Length"] == nil)
+        assert(same(body, {
+          "C\r\n",
+          "Hello ",
+          "World\n",
+          "\r\n0\r\n\r\n"
+        }))
+      end)
+      done()
+    end)
+
+  end)
+
+  describe("sync stream body", function ()
+    local stream = newStream()
+    local app = function (req, res)
+      res(200, {
+        ["Content-Type"] = "text/plain"
+      }, stream)
+    end
+    local request = {
+      method = "GET",
+      url = { path = "/" },
+      headers = {}
+    }
+    stream.write("Hello ")()
+    stream.write({"my ", "fun "})()
+    stream.write("World\n")()
+    stream.write()()
+
+    it("should do chunked encoding", function (done)
+      autoheaders(app)(request, function (code, headers, body)
+        assert(headers["Transfer-Encoding"] == "chunked")
+        assert(headers["Content-Length"] == nil)
+        assert(type(body) == "table")
+        assert(type(body.read) == "function")
+        local parts = {}
+        repeat
+          local chunk = await(body.read())
+          if chunk then
+            table.insert(parts, chunk)
+          end
+        until not chunk
+        assert(same({
+          { "6\r\n", "Hello ", "\r\n" },
+          { "7\r\n", "my ", "fun ", "\r\n" },
+          { "6\r\n", "World\n", "\r\n" },
+          "0\r\n\r\n"
+        }, parts))
+      end)
+      done()
+    end)
+
+  end)
+
+  describe("async stream body", function ()
+    local stream = newStream()
+    local app = function (req, res)
+      res(200, {
+        ["Content-Type"] = "text/plain"
+      }, stream)
+    end
+    local request = {
+      method = "GET",
+      url = { path = "/" },
+      headers = {}
+    }
+
+    it("should do chunked encoding", function (done)
+      autoheaders(app)(request, function (code, headers, body)
+        assert(headers["Transfer-Encoding"] == "chunked")
+        assert(headers["Content-Length"] == nil)
+        assert(type(body) == "table")
+        assert(type(body.read) == "function")
+        local parts = {}
+        repeat
+          local chunk = await(body.read())
+          if chunk then
+            table.insert(parts, chunk)
+          end
+        until not chunk
+        assert(same({
+          { "6\r\n", "Hello ", "\r\n" },
+          { "7\r\n", "my ", "fun ", "\r\n" },
+          { "6\r\n", "World\n", "\r\n" },
+          "0\r\n\r\n"
+        }, parts))
+      end)
+      done()
+    end)
+
+    local input = {
+      "Hello ",
+      {"my ", "fun "},
+      "World\n"
+    }
+    local index = 1
+    local function next()
+      local message = input[index]
+      index = index + 1
+      stream.write(message)(function ()
+        if message then
+          nextTick()(next)
+        end
+      end)
+    end
+    nextTick()(next)
+
+  end)
+
+
+end)
+
+flushTickQueue()
