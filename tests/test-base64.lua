b9c2095b2baaed9a5e848043686f9e06ba673046
Dennis Schridde <devurandom@gmx.net>
2012-12-09 20:28:25 +0100

    Base64 module with unit-test
    * Encode function copied from websocket.lua
      -> Directly use it there

---
+++ tests/test-base64.lua
@@ -0,0 +1,16 @@
+local base64 = require "base64"
+local describe = require('ensure').describe
+
+local dec = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure."
+local enc = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4="
+
+describe("base64", function()
+	it("encodes properly", function(done)
+		assert(base64.encode(dec) == enc)
+		done()
+	end)
+	it("decodes properly", function(done)
+		assert(base64.decode(enc) == dec)
+		done()
+	end)
+end)
